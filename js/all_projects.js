13189115e739a7cd608966550f7621756795178b
dormammuuuuu <78258443+dormammuuuuu@users.noreply.github.com>
2021-02-17 23:07:33 +0800

    stable 2-17-21

---
+++ js/all_projects.js
@@ -0,0 +1,15 @@
+function viewTab(evt, tabName){
+  var i, content, button_status;
+  content = document.getElementsByClassName("description");
+  for (i = 0; i < content.length; i++) {
+    content[i].style.display = "none";
+  }
+  button_status = document.getElementsByClassName("proj_buttons_label");
+  for (i = 0; i < button_status.length; i++) {
+    button_status[i].className = button_status[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+  }
+
+  document.getElementById("default_tab").click();
---
+++ projects/projects.js
@@ -0,0 +1,24 @@
+var modal = document.getElementById('modal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName('description_image');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+
+var showModal = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+for (var i = 0; i < img.length; i++) {
+    img[i].addEventListener('click', showModal);
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
