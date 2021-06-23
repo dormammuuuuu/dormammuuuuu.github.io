786252567b472fc71d71b74f938b04e699a9e9b4
dormammuuuuu <jericovic64@gmail.com>
2021-06-23 20:36:25 +0800

    Carousel

--- js/all_projects.js
+++ js/all_projects.js
@@ -1,4 +1,45 @@
-function viewTab(evt, tabName) {
+var galleryCarousel = document.querySelector('#galleryCarousel')
+var carousel = new bootstrap.Carousel(galleryCarousel, {
+  interval: 1500,
+  wrap: false
+})
+
+var triggerTabList = [].slice.call(document.querySelectorAll('#tabs a'))
+triggerTabList.forEach(function (triggerEl) {
+  var tabTrigger = new bootstrap.Tab(triggerEl)
+
+  triggerEl.addEventListener('click', function (event) {
+    tabTrigger.show()
+  })
+})
+
+var myCarousel = document.querySelector('#galleryCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+})
+
+function startUp(){
+  setHeight= $(".carousel-inner img:nth-child(1)").height();
+  $(".carousel-inner").height(setHeight)
+  $(".carousel-inner img").each(function(){
+     $(this).css('max-height', setHeight);
+  })
+}
+
+
+
+
+
+/*function viewTab(evt, tabName) {
+function scrollSpy(){
+  var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+    target: '#tabs'
+  });
+
+  hotfixScrollSpy();
+  window.scrollBy(0,1);
+}
   var i, content, button_status;
   content = document.getElementsByClassName("description");
   for (i = 0; i < content.length; i++) {
@@ -13,3 +54,4 @@ function viewTab(evt, tabName) {
 }
 
 document.getElementById("default_tab").click();
+*/
